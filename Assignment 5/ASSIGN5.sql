595eaef53945e175e9a2157ba0c4bfe860ee692c
Hà Văn Hanh <48597221+HaVanHanh@users.noreply.github.com>
2019-10-26 20:49:09 +0700

    Update ASSIGN5.sql

--- Assignment 5/ASSIGN5.sql
+++ Assignment 5/ASSIGN5.sql
@@ -4,7 +4,7 @@
 */
 
 
-USE adventureworks;
+USE 		adventureworks;
 SELECT  	P.Name
 FROM 		Product AS P
 WHERE	 	P.ProductSubcategoryID
@@ -55,10 +55,10 @@ AdventureWorks2008sample database.
 */
 USE 			adventureworks;
 SELECT 			Cou.Name 				AS 	Country ,
-				Provi.Name 				AS	Province
-FROM 			countryregion			AS 	Cou
-INNER JOIN 		stateprovince 			AS 	Provi
-ON 				Cou.CountryRegionCode	=	Provi.CountryRegionCode;
+			Provi.Name 				AS	Province
+FROM 			countryregion				AS 	Cou
+INNER JOIN 		stateprovince 				AS 	Provi
+ON 			Cou.CountryRegionCode	=		Provi.CountryRegionCode;
 	
 
 /*
@@ -69,13 +69,13 @@ Chú ý: sủ dụng sort order và column headings
 
 USE 			adventureworks;
 SELECT 			Cou.Name 						AS Country ,
-				Provi.Name 						AS	Province
-FROM 			countryregion					AS Cou
-INNER JOIN 		stateprovince 					AS Provi
+			Provi.Name 						AS	Province
+FROM 			countryregion						AS Cou
+INNER JOIN 		stateprovince 						AS Provi
 		
-ON 				Cou.CountryRegionCode	=		Provi.CountryRegionCode
-GROUP BY					Provi.Name,			Cou.CountryRegionCode
-HAVING 			Cou.Name ="Germany" 			OR Cou.Name ="Canada";
+ON 			Cou.CountryRegionCode	=	Provi.CountryRegionCode
+GROUP BY		Provi.Name,			Cou.CountryRegionCode
+HAVING 			Cou.Name ="Germany" 		OR Cou.Name ="Canada";
 
 /*
 Question 3:
@@ -87,7 +87,7 @@ BusinessEntityID (là định danh của người sales), Bonus and the SalesYTD
 SELECT 		SOH.SalesOrderID,	SOH.OrderDate,		S.SalesPersonID , S.Bonus,s.SalesYTD
 FROM 		SalesPerson 		AS S		
 INNER JOIN 	SalesOrderHeader	AS SOH
-ON 			S.SalesPersonID		=SOH.SalesPersonID
+ON 		S.SalesPersonID		=SOH.SalesPersonID
 ORDER BY	SOH.SalesOrderID	ASC
 
 /*
